44dc0e52a294548551fcdfe189415871f8d2f9c1
KowsalyaBaskaran <kowsalyabaskaran99@gmail.com>
2017-04-14 11:46:22 +0530

    Create pos.java

---
+++ pos.java
@@ -0,0 +1,19 @@
+import java.util.*;
+public class pos {
+
+    public static void main(String[] args) {
+int a;
+System.out.println("enter the number");
+Scanner in=new Scanner(System.in);
+a=in.nextInt();
+if(a>=0)
+{
+    System.out.println("positive");
+}
+else
+{
+    System.out.println("negative");
+}
+    }
+    
+}
