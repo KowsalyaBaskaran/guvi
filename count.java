d36bb958afbe4003b72d9770c4ab9255567f0cf6
KowsalyaBaskaran <kowsalyabaskaran99@gmail.com>
2017-04-16 21:54:22 +0530

    Create count.java

---
+++ count.java
@@ -0,0 +1,10 @@
+import java.util.*;
+public class count {
+    public static void main(String args[])
+    {
+        int numbers=123456;
+        int length=(int)Math.log10(numbers)+1;
+        System.out.println(length);
+        
+    }
+}
