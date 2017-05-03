df1a1154f413d1d70080e1a82ea15746950fc584
KowsalyaBaskaran <kowsalyabaskaran99@gmail.com>
2017-05-03 23:28:57 +0530

    Create newclass.java

---
+++ newclass.java
@@ -0,0 +1,10 @@
+import java.util.*;
+public class NewClass
+{
+public static void main(String args[])
+{
+String str="this is java programming" ;
+int temp=str.length();
+System.out.println(temp);
+}
+}
