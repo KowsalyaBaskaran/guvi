4db63734178b8a5d902b4ebe853dc59a4a173395
KowsalyaBaskaran <kowsalyabaskaran99@gmail.com>
2017-05-06 09:19:13 +0530

    Create ascii.java

---
+++ ascii.java
@@ -0,0 +1,12 @@
+import java.util.Arrays;
+import java.util.Scanner;
+class sivam
+{
+public static void main(String args[])
+{
+ int c;
+ for(c=32;c<=128;c++){
+     System.out.println(c+" "+(char)c);
+ }
+  
+}}
