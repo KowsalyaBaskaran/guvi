73ed2a8bd7e444951a56c87977f9aa3cb7616038
KowsalyaBaskaran <kowsalyabaskaran99@gmail.com>
2017-05-24 22:21:19 +0530

    Create string.java

---
+++ string.java
@@ -0,0 +1,9 @@
+import java.util.*;
+public class string {
+    public static void main(String args[])
+    {
+        String num="20";
+        int result = Integer.parseInt(num);
+        System.out.println(result);
+    }
+}
