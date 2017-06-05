a2c34833d09ab01a4bc04167855cdc37c242e3d4
KowsalyaBaskaran <kowsalyabaskaran99@gmail.com>
2017-06-05 21:40:37 +0530

    Create day.java

---
+++ day.java
@@ -0,0 +1,37 @@
+
+import java.util.*;
+public class day {
+    public static void main(String args[])
+    {
+        
+        Scanner in=new Scanner(System.in);
+        String day=in.next();
+        switch(day)
+        {
+            case "monday":
+                System.out.println("true");
+                 break;
+        case "tuesday":
+                System.out.println("true");
+                 break;
+        case "wednesday":
+                System.out.println("true");
+                 break;
+        case "thursday":
+                System.out.println("true");
+                 break;
+        case "friday":
+                System.out.println("true");
+                 break;
+        case "saturday":
+                System.out.println("true");
+                 break;
+        case "sunday":
+                System.out.println("false");
+                 break;
+        
+        
+        }
+        
+    }
+}
