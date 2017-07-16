fbf56ef3c508cf2bf8afeaac0b34944bdfda0bad
KowsalyaBaskaran <kowsalyabaskaran99@gmail.com>
2017-07-16 12:05:15 +0530

    Create com.java

---
+++ com.java
@@ -0,0 +1,17 @@
+import java.util.*;
+public class com {
+    public static void main(String args[])
+    {
+        Scanner in=new Scanner(System.in);
+        String a=in.next();
+        String b=in.next();
+        if(a==b)
+        {
+            System.out.print("yes");
+        }
+        else
+        {
+            System.out.print("no");
+        }
+    }
+}
