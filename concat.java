31ae11129703b166924f9cff032c3e603fcee52c
KowsalyaBaskaran <kowsalyabaskaran99@gmail.com>
2017-09-27 23:08:33 +0530

    Create concat.java

---
+++ concat.java
@@ -0,0 +1,11 @@
+import java.util.*;
+class concat
+{
+public static void main(String args[])
+{
+Scanner in=new Scanner(System.in);
+String a=in.nextLine();
+String b=in.nextLine();
+System.out.print(a+b);
+}
+}
