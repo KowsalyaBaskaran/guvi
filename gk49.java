3502b9bdd4e20c54ef6db785758155275012bcfd
KowsalyaBaskaran <kowsalyabaskaran99@gmail.com>
2017-09-27 23:32:23 +0530

    Create gk49.java

---
+++ gk49.java
@@ -0,0 +1,17 @@
+import java.util.*;
+public class gk49 {
+    public static void main(String args[])
+    {
+       Scanner in=new Scanner(System.in);
+       System.out.println("enter the table");
+       int a=in.nextInt();
+       System.out.println("enter the limit");
+       int b=in.nextInt();
+       //int c[]=new int[b];
+       for(int i=1;i<=b;i++)
+       {
+           int d=i*a;
+           System.out.println(i+"*"+a+"="+d);
+       }
+    }
+}
